9d4553e114febdb6b66149efcab1adfd2bff00e2
Peter Werry <pwerry@gmail.com>
2022-11-01 08:38:47 -0700

    Fails to build with missing files and module errors

---
+++ EmbeddedAppGRPCSwiftExample/EmbeddedApp/EmbeddedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmbeddedAppApp.swift
+//  EmbeddedApp
+//
+//  Created by Peter Werry on 2022-10-31.
+//
+
+import SwiftUI
+
+@main
+struct EmbeddedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmbeddedAppGRPCSwiftExample/HostApp/HostAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HostAppApp.swift
+//  HostApp
+//
+//  Created by Peter Werry on 2022-10-31.
+//
+
+import SwiftUI
+
+@main
+struct HostAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
